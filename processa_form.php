0067e50c568f014fca51ae774bcae34ad0d80e6c
Daudt <32130636+daudt86@users.noreply.github.com>
2021-01-20 17:44:05 -0300

    Update processa_form.php

--- processa_form.php
+++ processa_form.php
@@ -1,8 +1,14 @@
 <?php 
 
+#primeira etapa é recuperar as variaveis do formulário 
 
 
+# vou recuperar apenas um como exemplo
 
+$nome = $_POST['nome']; # note que, como estou utilizando o method POST, eu preciso usar a variavel global $_POST para recuperar o dado enviado pelo formulário. o parametro passado dentro da variavel $_POST deve ser exatamente o "name" do formulario, nesse exemplo o valor setado no formulario deve ser name='nome'.
 
+#Para imprimir e visualizar esse valor na tela 
+
+echo 'Nome digitado no formulario: ' . $nome # estou usando o " . " para concatenar o texto com a variavel
 
 ?>
